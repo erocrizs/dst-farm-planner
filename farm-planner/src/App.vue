07e3b68642e75dbb81f5b3adaba14421ea8e9ff6
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-03-02 19:09:03 +0800

    Add mobile view support

--- farm-planner/src/App.vue
+++ farm-planner/src/App.vue
@@ -6,11 +6,11 @@
     <div id="field-canvas-container">
       <FieldCanvas/>
     </div>
-    <div id="status-bar-container">
-      <StatusBar/>
-    </div>
     <div id="tool-bar-container">
       <ToolBar/>
+    </div>
+    <div id="status-bar-container">
+      <StatusBar/>
     </div>
   </div>
 </template>
@@ -40,23 +40,44 @@ export default {
 
   #app {
     height: 100%;
-    display: grid;
-    grid-template: 50px 1fr 50px / 1fr 500px;
   }
 
   #navigation-bar-container {
-    grid-area: 1/1/span 1/span 2;
+    height: 50px;
   }
 
   #field-canvas-container {
-    grid-area: 2/1/span 2/span 1;
+    height: 100vw;
   }
 
   #status-bar-container {
-    grid-area: 2/2/span 2/span 1;
+    min-height: calc(100vh - 100px - 100vw);
   }
 
   #tool-bar-container {
-    grid-area: 3/1/span 1/span 1;
+    height: 50px;
+  }
+
+  @media screen and (min-width: 780px) {
+    #app {  
+      display: grid;
+      grid-template: 50px 1fr 50px / 1fr 300px;
+    }
+
+    #navigation-bar-container {
+      grid-area: 1/1/span 1/span 2;
+    }
+
+    #field-canvas-container {
+      grid-area: 2/1/span 2/span 1;
+    }
+
+    #status-bar-container {
+      grid-area: 2/2/span 2/span 1;
+    }
+
+    #tool-bar-container {
+      grid-area: 3/1/span 1/span 1;
+    }
   }
 </style>
