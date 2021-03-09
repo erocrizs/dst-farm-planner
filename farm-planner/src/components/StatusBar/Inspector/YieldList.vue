e73c7375f5551292f2e928c45a473ec23cf6bc1d
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-03-09 23:57:14 +0800

    Implement YieldList

--- farm-planner/src/components/StatusBar/Inspector/YieldList.vue
+++ farm-planner/src/components/StatusBar/Inspector/YieldList.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="yield-list">
-    Yield List: {{JSON.stringify(yields)}}
+    <div class="yield-count"
+      v-for="(yieldCount, index) in yieldList"
+      :key="index">
+      <img class="yield-img" :src="yieldCount.imgSrc"/>
+      x {{yieldCount.count}}
+    </div>
   </div>
 </template>
 
 <script>
+import crops from '@/library/crops'
+
 export default {
   name: 'YieldList',
   props: ['yields'],
   data () {
-    return {}
+    const yieldList = []
+
+    for (let crop in this.yields) {
+      yieldList.push({
+        imgSrc: './static/' + crops[crop].src.normal,
+        count: this.yields[crop].crop
+      })
+
+      yieldList.push({
+        imgSrc: './static/' + crops[crop].src.seeds,
+        count: this.yields[crop].seed
+      })
+    }
+
+    return {yieldList}
   }
 }
 </script>
@@ -17,5 +38,19 @@ export default {
 <style scoped>
 .yield-list {
   font-size: 0.8em;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: flex-start;
+}
+
+.yield-count {
+  margin-right: 15px;
+}
+
+.yield-count img {
+  display: inline-block;
+  height: 1em; 
 }
 </style>
