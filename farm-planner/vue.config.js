e259086320191527fba4c1a905fb7fa203f288f8
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-03-11 11:48:37 +0800

    Add some SEO meta stuff

--- farm-planner/vue.config.js
+++ farm-planner/vue.config.js
@@ -10,5 +10,21 @@ module.exports = {
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dst-farm-planner/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Don\'t Starve Together Farm Planner',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  }
 }
